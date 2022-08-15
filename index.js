7644ac2489d047112fd75f6527ff2265684dc51c
Hieser21 <rarafat883@gmail.com>
2022-08-15 20:51:05 +0600

    Revert "Update index.js"
    This reverts commit 06a55149a4aee13d59a42b5739681a6c3635fc56.

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const { google } = require('googleapis')
 const OAuth2 = google.auth.OAuth2
 
 const oauth2Client = new OAuth2(
-    "758541214200-6cflh3bf8aqomqr332qvv4fcriapppo4.apps.googleusercontent.com",
-    "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",
+    process.env.Client_ID,
+    process.env.Client_Secret,
     "https://developers.google.com/oauthplayground"
     )
 
 oauth2Client.setCredentials({
-    refresh_token:"1//04QsHIKnN49jyCgYIARAAGAQSNwF-L9IrQuPYuIb2dRdp8AN1zqR7wRTr_puvX9MDia5ir71yUt3lryzvr9oRYXUXYMqQN0l6Eao"
+    refresh_token:process.env.Refresh_Token
 })
 const accessToken = oauth2Client.getAccessToken()
 
@@ -41,15 +41,15 @@ const smtpTrans = nodemailer.createTransport({
     secure: true,
   auth:{
   type:"OAuth2",
-  user:"rarafat883@gmail.com",
-  clientId:"758541214200-6cflh3bf8aqomqr332qvv4fcriapppo4.apps.googleusercontent.com",
-  clientSecret:"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",
-  refreshToken:"1//04QsHIKnN49jyCgYIARAAGAQSNwF-L9IrQuPYuIb2dRdp8AN1zqR7wRTr_puvX9MDia5ir71yUt3lryzvr9oRYXUXYMqQN0l6Eao",
+  user:process.env.GMAIL_USER,
+  clientId:process.env.Client_ID,
+  clientSecret:process.env.Client_Secret,
+  refreshToken:process.env.Refresh_Token,
   accessToken:accessToken
   }})
 const mailOpts = {
-  from:"rarafat883@gmail.com",
-  to:"rarafat15@yahoo.com"
+  from:process.env.GMAIL_USER,
+  to:process.env.RECIPIENT,
   subject:'New message from Nodemailer-contact-form',
   html:output
 }
